80e06c7202990aff5c11d582d2f57a6815c3153f
Adetops <heritagediwamu@gmail.com>
2023-05-22 21:30:18 -0700

    Started

---
+++ 1-shell.c
@@ -0,0 +1,76 @@
+#include "shell.h"
+
+/**
+ * prompt - prompts the user for commands
+ * @ptr: ptr will hold the string of commands
+ * Return: 1 for success
+ */
+int prompt(char **ptr)
+{
+	size_t size = 0;
+	int len;
+
+	if (isatty(STDIN_FILENO))
+		write(STDOUT_FILENO, "Heritage$ ", 10);
+	len = getline(ptr, &size, stdin);
+	if (len == EOF)
+		free_cptrn(-1, 1, *ptr);
+	(*ptr)[len - 1] = '\0';
+	if (*(*ptr) == '\0' || (*(*ptr) == '.' && (*ptr)[1] == '\0'))
+	{
+		free_cptrn(99, 1, *ptr);
+		return (-1);
+	}
+	else
+		return (1);
+}
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: list of arguments
+ * @envp: list of environment variables
+ * Return: Always 0
+ */
+int main(__attribute__((unused)) int argc, char **argv, char **envp)
+{
+	char *ptr = NULL;
+	char **string = NULL;
+	int status;
+	pid_t parent;
+
+	while (1)
+		{
+		ptr = NULL;
+		signal(SIGINT, signal_handler);
+		if (prompt(&ptr) == -1)
+			continue;
+		string = strtow(ptr);
+		if (!string)
+		{
+			free_cptrn(99, 1, ptr);
+			continue;
+		}
+		free_cptrn(99, 1, ptr);
+		if (herit_built(string, envp))
+			continue;
+		parent = fork();
+		if (parent == 0)
+		{
+			check_path(string, envp);
+			if (execve(string[0], string, NULL) == -1)
+			{
+				perror(*argv);
+				free_array(string);
+				exit(0);
+			}
+		}
+		else
+		{
+			free_array(string);
+			if (!wait(&status))
+				break;
+		}
+	}
+	return (0);
+}
---
+++ shell.h
@@ -0,0 +1,47 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/wait.h>
+
+/**
+ * struct builtins - builtin command and functions for shell
+ * @cmd: command name
+ * @f: function pointer to builtin functions
+ */
+typedef struct builtins
+{
+	char *cmd;
+	int (*f)(char **args, char **env);
+} built_t;
+
+int prompt(char **ptr);
+
+/* Function declarations for builtin shell commands */
+int herit_env(char **args, char **envp);
+int herit_exit(char **args, char **envp);
+int herit_built(char **args, char **envp);
+
+void free_array(char **array);
+void free_cptrn(int, const unsigned int n, ...);
+char *str_concat(char *s1, char *s2);
+void check_path(char **string, char **envp);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_getenv(const char *name, char **envp);
+int _strcmp2(const char *s1, const char *s2);
+char **strtow(char *str);
+void get_word(char **array, char *str);
+int _strlen(char *s);
+int _strcmp(const char *s1, const char *s2);
+int word_count(char *str);
+void cpy_str(int end, char *s, char **word_box);
+void signal_handler(int signo);
+void err_exit(char *, int status);
+int _atoi(char *s);
+
+#endif
