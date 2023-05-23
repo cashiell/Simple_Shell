460faf8276724c1b30a194a4e3c5da06bec3f727
Adetops <heritagediwamu@gmail.com>
2023-05-23 02:41:51 -0700

    environment variables

---
+++ 5-env.c
@@ -0,0 +1,12 @@
+#include "shell.h"
+
+/**
+ * herban_envp - prints environment variables
+ * @args: array of argument
+ * @envp: array of environment variables
+ * Return: 1 on success
+ */
+int herban_envp(char ** args, char **envp)
+{
+
+}
