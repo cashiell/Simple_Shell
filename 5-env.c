244273a529e30e94148695c45ab075367a81f41d
Adetops <heritagediwamu@gmail.com>
2023-05-23 06:34:00 -0700

    env and exit builtin commands

--- 5-env.c
+++ 5-env.c
@@ -8,5 +8,43 @@
  */
 int herban_envp(char ** args, char **envp)
 {
+	int i = 0, len;
 
+	while (envp[i] != NULL)
+	{
+		len = _strlen(envp[i]);
+		write(STDOUT_FILENO, envp[i], len);
+		write(STDOUT_FILENO, "\n", 1);
+		i++;
+	}
+	fre_array(args);
+	return (1);
+}
+
+/**
+ * herban_built - checks for builtin commands and executes them
+ * @args: arrays of arguments
+ * @envp: array of environment variables
+ * Return: 1 on success
+ */
+int herban_built(char **args, char **envp)
+{
+	built_t b_ins[] = {
+		{"env", herban_env},
+		{"exit", herban_exit},
+		{NULL, NULL}
+	};
+	int i;
+
+	if (args[0] == NULL)
+		return (0);
+	for (i = 0; i < 2; i++)
+	{
+		if (_strcmp2(args[0], b_ins[i].cmd) == 0)
+		{
+			return (b_ins[i].f(args, envp));
+			break;
+		}
+	}
+	return (0);
 }
